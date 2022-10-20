f42b19fe1eb8af3ddac11a157d137cb6ccbea22f
ep-scq <ep_scq@hotmail.com>
2022-10-19 23:02:52 -0300

    Segunda Aula Front-end-3

---
+++ front-end-III/digital-house/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,24 @@
+import './style.scss'
+
+export function SegundaAula() {
+    return (
+        <main>
+            <h1>Segunda Aula</h1>
+            <h2>Projeto escolhido: Youtube</h2>
+            <ul>
+                <li>
+                    <h1>Primeiro componente</h1>
+                    <p>Botão de filtro</p>
+                </li>
+                <li>
+                    <h1>Segundo componente</h1>
+                    <p>Título do card</p>
+                </li>
+                <li>
+                    <h1>Terceiro componente</h1>
+                    <p>Ícone do sininho</p>
+                </li>
+            </ul>
+        </main>
+    )
+}
